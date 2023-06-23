929f6a19c1796aa56a2f2b68279b382903a6a87c
mjolsic <wongqianjie1234@gmail.com>
2023-06-24 09:26:36 +1000

    1) Added new Blossom enum types 2) Added more Blossom related data.

---
+++ src/main/java/emu/grasscutter/game/managers/blossom/enums/BlossomClientShowType.java
@@ -0,0 +1,7 @@
+package emu.grasscutter.game.managers.blossom.enums;
+
+public enum BlossomClientShowType {
+    BLOSSOM_SHOWTYPE_CHALLENGE,
+    BLOSSOM_SHOWTYPE_NPCTALK,
+    BLOSSOM_SHOWTYPE_GROUPCHALLENGE
+}
