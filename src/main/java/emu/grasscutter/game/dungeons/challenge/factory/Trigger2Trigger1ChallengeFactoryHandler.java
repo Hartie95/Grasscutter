8651cb04e4a40169e5d6c8fdb8caa4b7ab17d42c
mjolsic <wongqianjie1234@gmail.com>
2023-07-14 09:42:24 +1000

    1) Updated CHALLENGE_TRIGGER2_AVOID_TRIGGER1

--- src/main/java/emu/grasscutter/game/dungeons/challenge/factory/Trigger2Trigger1ChallengeFactoryHandler.java
+++ src/main/java/emu/grasscutter/game/dungeons/challenge/factory/Trigger2Trigger1ChallengeFactoryHandler.java
@@ -4,7 +4,6 @@ import emu.grasscutter.game.dungeons.challenge.ChallengeInfo;
 import emu.grasscutter.game.dungeons.challenge.ChallengeScoreInfo;
 import emu.grasscutter.game.dungeons.challenge.WorldChallenge;
 import emu.grasscutter.game.dungeons.challenge.enums.ChallengeType;
-import emu.grasscutter.game.dungeons.challenge.trigger.TimeTrigger;
 import emu.grasscutter.game.world.Scene;
 import emu.grasscutter.scripts.data.SceneGroup;
 
@@ -13,20 +12,21 @@ import java.util.List;
 public class Trigger2Trigger1ChallengeFactoryHandler implements ChallengeFactoryHandler {
     @Override
     public boolean isThisType(ChallengeType challengeType) {
+        // Start challenge with 666,203,{ 26,888,8,2,999,1 }
         return challengeType == ChallengeType.CHALLENGE_TRIGGER2_AVOID_TRIGGER1;
     }
 
     /**
      * Build a new challenge
-     * @param params: [timeLimit, triggerTag2, triggerCount2, unused1, triggerTag1, triggerCount1]
+     * @param params: [platformGoalPoint, triggerTag2, triggerCount2, unused1, triggerTag1, triggerCount1]
      */
     @Override
     public WorldChallenge build(ChallengeInfo header, List<Integer> params, ChallengeScoreInfo scoreInfo, Scene scene, SceneGroup group) {
         return new WorldChallenge(
             scene, group,
             header,
-            List.of(params.get(0)), // parameters
-            List.of(new TimeTrigger(1, params.get(0))),
+            List.of(), // parameters
+            List.of(),
             scoreInfo
         );
     }
